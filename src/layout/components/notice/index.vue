237504c1174915c2c20f12b4a54168968eabbeda
xyc <1365884247@qq.com>
2020-12-08 10:43:13 +0800

    修改notice样式

--- src/layout/components/notice/index.vue
+++ src/layout/components/notice/index.vue
@@ -1,18 +1,21 @@
 <template>
-  <div>
-<van-notice-bar
-  left-icon="volume-o"
-  text="在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准。"
-/>
+  <div class="notice">
+    <van-notice-bar
+      left-icon="volume-o"
+      text="在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准。"
+    />
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="less" scoped>
+.notice {
+  .van-notice-bar {
+    background-color: #ff3b3b;
+    color: #ffffff;
+  }
+}
 </style>
