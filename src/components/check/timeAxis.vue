175a0f3cc5e3ed7cc13dd9a9577040c1d7122c49
LZ <2706253331@qq.com>
2020-12-09 14:38:09 +0800

    完成点检页面

---
+++ src/components/check/timeAxis.vue
@@ -0,0 +1,347 @@
+<template>
+  <div class="block">
+    <div class="time">
+      <!-- 右背景 -->
+      <div class="bgright"></div>
+      <div class="timeScale"></div>
+      <span class="timeSpan">15:00:00</span>
+      <div class="bottomOverFlow">
+        <div class="ruler topcm">
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+        </div>
+
+        <div class="ruler bottomcm">
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm">
+            <div class="mm"></div>
+          </div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm">
+            <!-- 每一格刻度线 -->
+            <!-- <div class='mm'></div>
+            <div class='mm'></div>
+            <div class='mm'></div>
+            <div class='mm'></div>
+            <div class='mm'></div> -->
+          </div>
+          <div class="cm"></div>
+
+          <div class="cm"></div>
+          <div class="cm"></div>
+
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+          <div class="cm"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: 5.5
+    }
+  },
+  methods: {
+    formatTooltip(val) {
+      let hour = 0
+      let min = 0
+      const ms = val * 3600
+      hour = parseInt(ms / 3600)
+      if (hour < 10) {
+        hour = '0' + hour.toString()
+      } else {
+        hour = hour.toString()
+      }
+      min = ms % 3600
+      if (min === 0) {
+        min = '00'
+      } else {
+        min = '30'
+      }
+      const time = hour + ':' + min
+      return time
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.block {
+  width: 100%;
+}
+/deep/.el-slider__button {
+  width: 0px;
+  height: 63px;
+  border-radius: 0%;
+  border: 1px solid #409eff;
+  background-color: #409eff;
+}
+/deep/.el-slider__bar {
+  background-color: #777777;
+  height: 100%;
+}
+/deep/.el-slider__runway {
+  height: 56px;
+  background-color: #aaaaaa;
+}
+/deep/.el-slider__button-wrapper {
+  height: 50px;
+  display: none;
+}
+.ruler {
+  position: relative;
+  width: 100%;
+  margin: -16px auto;
+  height: 14px;
+}
+/* .ruler .mm  */
+.ruler .cm {
+  position: absolute;
+  border-left: 1px solid #555;
+  height: 14px;
+}
+.ruler .cm:after {
+  position: absolute;
+  bottom: 20px;
+  font: 11px/1 sans-serif;
+  color: #fff;
+}
+.ruler .cm:nth-of-type(1) {
+  left: 0%;
+}
+.bottomcm.ruler .cm:nth-of-type(1):after {
+  content: '00:00';
+}
+.ruler .cm:nth-of-type(2) {
+  left: 9.09091%;
+}
+.bottomcm.ruler .cm:nth-of-type(2):after {
+  content: '01:00';
+}
+.ruler .cm:nth-of-type(3) {
+  left: 18.1818%;
+}
+.bottomcm.ruler .cm:nth-of-type(3):after {
+  content: '02:00';
+}
+.ruler .cm:nth-of-type(4) {
+  left: 27.2727%;
+}
+.bottomcm.ruler .cm:nth-of-type(4):after {
+  content: '03:00';
+}
+.ruler .cm:nth-of-type(5) {
+  left: 36.3636%;
+}
+.bottomcm.ruler .cm:nth-of-type(5):after {
+  content: '04:00';
+}
+.ruler .cm:nth-of-type(6) {
+  left: 45.4545%;
+}
+.bottomcm.ruler .cm:nth-of-type(6):after {
+  content: '05:00';
+}
+.ruler .cm:nth-of-type(7) {
+  left: 54.5455%;
+}
+.bottomcm.ruler .cm:nth-of-type(7):after {
+  content: '06:00';
+}
+.ruler .cm:nth-of-type(8) {
+  left: 63.6364%;
+}
+.bottomcm.ruler .cm:nth-of-type(8):after {
+  content: '07:00';
+}
+.ruler .cm:nth-of-type(9) {
+  left: 72.7273%;
+}
+.bottomcm.ruler .cm:nth-of-type(9):after {
+  content: '08:00';
+}
+.ruler .cm:nth-of-type(10) {
+  left: 81.8182%;
+}
+.bottomcm.ruler .cm:nth-of-type(10):after {
+  content: '09:00';
+}
+.ruler .cm:nth-of-type(11) {
+  left: 90.9091%;
+}
+.bottomcm.ruler .cm:nth-of-type(11):after {
+  content: '10.00';
+}
+.ruler .cm:nth-of-type(12) {
+  left: 100%;
+}
+.bottomcm.ruler .cm:nth-of-type(12):after {
+  content: '11.00';
+}
+
+.ruler .cm:nth-of-type(13) {
+  left: 109.09091%;
+}
+.bottomcm.ruler .cm:nth-of-type(13):after {
+  content: '12.00';
+}
+
+.ruler .cm:nth-of-type(14) {
+  left: 118.1818%;
+}
+
+.bottomcm.ruler .cm:nth-of-type(14):after {
+  content: '13.00';
+}
+.ruler .cm:nth-of-type(15) {
+  left: 127.2727%;
+}
+.bottomcm.ruler .cm:nth-of-type(15):after {
+  content: '14.00';
+}
+.ruler .cm:nth-of-type(16) {
+  left: 136.3636%;
+}
+.bottomcm.ruler .cm:nth-of-type(16):after {
+  content: '15.00';
+}
+.ruler .cm:nth-of-type(17) {
+  left: 145.4545%;
+}
+.bottomcm.ruler .cm:nth-of-type(17):after {
+  content: '16.00';
+}
+.ruler .cm:nth-of-type(18) {
+  left: 154.5455%;
+}
+.bottomcm.ruler .cm:nth-of-type(18):after {
+  content: '17.00';
+}
+.ruler .cm:nth-of-type(19) {
+  left: 163.6364%;
+}
+.bottomcm.ruler .cm:nth-of-type(19):after {
+  content: '18.00';
+}
+.ruler .cm:nth-of-type(20) {
+  left: 172.7273%;
+}
+.bottomcm.ruler .cm:nth-of-type(20):after {
+  content: '19.00';
+}
+.ruler .cm:nth-of-type(21) {
+  left: 181.8182%;
+}
+.bottomcm.ruler .cm:nth-of-type(21):after {
+  content: '20.00';
+}
+.ruler .cm:nth-of-type(22) {
+  left: 190.9091%;
+}
+.bottomcm.ruler .cm:nth-of-type(22):after {
+  content: '21.00';
+}
+
+#mentionme {
+  text-align: center;
+  margin-top: 10%;
+}
+.topcm {
+  position: absolute;
+  top: 0;
+  z-index: 1;
+  margin: 0;
+}
+.bottomcm {
+  position: absolute;
+  margin: 0;
+  bottom: 0;
+}
+.el-tooltip__popper {
+  opacity: 0 !important;
+}
+.bottomOverFlow {
+  width: 100%;
+  height: 56px;
+  position: relative;
+  overflow: scroll;
+  // top: -72px;
+}
+.bottomOverFlow::-webkit-scrollbar,
+.topcm::-webkit-scrollbar,
+.topcm::-webkit-scrollbar {
+  width: 0;
+  height: 0;
+  background-color: #f5f5f5;
+  opacity: 0;
+}
+.timeScale {
+  position: absolute;
+  width: 2px;
+  height: 108%;
+  background-color: #3299ff;
+  left: 50%;
+  top: -4px;
+}
+.time {
+  position: relative;
+}
+.timeSpan {
+  position: absolute;
+  color: #61afff;
+  left: 50%;
+  transform: translateX(-50%);
+  top: -25px;
+  font-size: 12px;
+}
+.bgright {
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 50%;
+  height: 56px;
+  background-color: #aaaaaa;
+}
+</style>
--- src/views/check/index.vue
+++ src/views/check/index.vue
@@ -1,19 +1,160 @@
 <template>
-  <div>
+  <div class="check">
     <NavBar></NavBar>
-    点检页面
+    <!-- 摄像头button区域 -->
+    <div class="camera">
+      <div
+        :class="`cameraButton  ${defaultStyle ? 'defaultStyle' : ''}`"
+        @click="btn"
+      >
+        <van-button v-for="(item, i) of cameraData" :key="i">{{
+          item
+        }}</van-button>
+      </div>
+      <!-- 视屏 -->
+      <div class="video">
+        <img :src="video" />
+      </div>
+      <!-- 日期选择区域 -->
+      <div class="detaButton">
+        <i class="iconfont iconxiala-yuan-tianchong" />
+        <van-button v-for="(item, i) of detas" :key="i" type="default">{{
+          item
+        }}</van-button>
+      </div>
+      <!-- 时间轴 -->
+      <div class="timeAxis">
+        <TimeAxis></TimeAxis>
+      </div>
+      <div class="checkBtn">
+        <van-button to="CheckItem">点检</van-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import NavBar from '@/layout/components/navBar'
+import TimeAxis from '@/components/check/timeAxis.vue' // 时间轴
 export default {
   components: {
-    NavBar
+    NavBar,
+    TimeAxis
+  },
+  data() {
+    return {
+      // tab选项默认选中项
+      defaultStyle: true,
+      // 摄像头数据
+      cameraData: [
+        '大厅摄像头',
+        '主厅摄像头',
+        '副厅摄像头',
+        '摄像头1',
+        '大厅摄像头背后1',
+        '主厅摄像头背后6'
+      ],
+      // 视屏数据
+      video: require('@/assets/screenImage/videoImage.png'),
+      detas: ['11-22', '11-23', '11-24', '前天', '昨天', '今天', '现在']
+    }
+  },
+  methods: {
+    // 清除默认选中属性
+    btn() {
+      this.defaultStyle = false
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="less" scoped>
+.check {
+  padding-bottom: 56px;
+  background-color: #eeeeee;
+}
+.camera {
+  margin: 15px 15px 0;
+  padding-top: 15px;
+  background-color: #fff;
+  border-radius: 8px;
+  overflow: hidden;
+}
+// 摄像头button区域样式
+.cameraButton {
+  display: flex;
+  overflow-y: scroll;
+}
+.cameraButton::-webkit-scrollbar {
+  width: 0;
+  height: 0;
+}
+.cameraButton > .van-button {
+  flex-shrink: 0;
+  font-size: 12px;
+  border: 1px solid #dcdfe6;
+  border-radius: 4px;
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+}
 
+.van-button:hover {
+  border-bottom: 0;
+  color: #409eff;
+}
+.defaultStyle > .van-button:first-child {
+  border-bottom: 0;
+  color: #409eff;
+}
+
+// 视屏区域样式
+.video {
+  margin: 15px 0;
+}
+.video > img {
+  width: 100%;
+}
+// 日期选择样式
+.detaButton {
+  display: flex;
+  justify-content: space-between;
+}
+.detaButton > .van-button {
+  font-size: 12px;
+  padding: 0 8px;
+  height: 32px;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+}
+
+// 阿里图标样式
+.iconxiala-yuan-tianchong {
+  color: #4490d2;
+  font-size: 28px;
+  margin-left: 9px;
+}
+// 时间轴样式
+.timeAxis {
+  margin: 40px 0 62px;
+  background-color: #777777;
+}
+// 点检样式
+.checkBtn {
+  background-color: #eff2f5;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.checkBtn > .van-button {
+  width: 120px;
+  height: 120px;
+  // border: 1px solid red;
+  border-radius: 50%;
+  box-shadow: 0px 0px 8px #d4cdcd;
+  color: #3c9dff;
+  outline: none;
+  font-size: 30px;
+  color: #3c9dff;
+}
 </style>
