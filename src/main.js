76f4727912a1e8067562f5f995dba5913c748f2a
xyc <1365884247@qq.com>
2020-12-10 14:38:22 +0800

    添加首页矢量图

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import Vant from 'vant'
 import 'vant/lib/index.css'
 import '@/assets/css/reset.css'
 import '@/assets/iconfont/iconfont.css'
+// 引入全局矢量图
+import '@/assets/icon/iconfont.css'
 
 // 引入echarts挂载全局
 import * as echarts from 'echarts'
