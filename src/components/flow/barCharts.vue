7069a84bff1bd401b721ecf865363c74cdb57356
xyc <1365884247@qq.com>
2020-12-12 16:37:52 +0800

    完成客流界面-客群特征-客流与销售分析

---
+++ src/components/flow/barCharts.vue
@@ -0,0 +1,257 @@
+<template>
+  <div class="barCharts">
+    <div id="barCharts" style="width: 100%;height: 350px;"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    option: {
+      // 所有配置
+      type: Object,
+      default: null
+    },
+    // 图的label
+    label: {
+      type: Array,
+      default: function() {
+        return []
+      }
+    },
+    // x轴的label
+    xLabel: {
+      type: Array,
+      default: function() {
+        return []
+      }
+    },
+    // x轴的名字
+    xName: {
+      type: String,
+      default: ''
+    },
+    // 第一条y轴的名字
+    yName1: {
+      type: String,
+      default: ''
+    },
+    // 第二条y轴的名字
+    yName2: {
+      type: String,
+      default: ''
+    },
+    // 第一条柱状图的名字
+    dataName1: {
+      type: String,
+      default: ''
+    },
+    // 第一条柱状图的数据
+    data1: {
+      type: Array,
+      default: function() {
+        return []
+      }
+    },
+    // 第二条柱状图的名字
+    dataName2: {
+      type: String,
+      default: ''
+    },
+    // 第二条柱状图的数据
+    data2: {
+      type: Array,
+      default: function() {
+        return []
+      }
+    },
+    // 第三条条柱状图的名字
+    dataName3: {
+      type: String,
+      default: ''
+    },
+    // 第三条柱状图的数据
+    data3: {
+      type: Array,
+      default: function() {
+        return []
+      }
+    }
+  },
+  methods: {
+    drawChart() {
+      // 基于准备好的dom，初始化echarts实例
+      const myChart = this.$echarts.init(document.getElementById('barCharts'))
+      let option = null
+      if (this.option) {
+        option = this.option
+      } else {
+        // 指定图表的配置项和数据
+        option = {
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross',
+              crossStyle: {
+                color: '#999'
+              }
+            }
+          },
+          legend: {
+            data: this.label, // 轴代表的数据label
+            right: 25, // 偏移
+            top: 10,
+            icon: 'circle', //  这个字段控制形状  类型包括 circle，rect ，roundRect，triangle，diamond，pin，arrow，none
+            itemWidth: 10, // 设置宽度
+            itemHeight: 10, // 设置高度
+            itemGap: 20 // 设置间距
+          },
+          xAxis: [
+            {
+              type: 'category',
+              data: this.xLabel,
+              name: this.xName,
+              axisLine: {
+                lineStyle: {
+                  // 设置坐标轴线的颜色
+                  color: '#707070'
+                }
+              },
+              axisPointer: {
+                type: 'shadow'
+              },
+              axisTick: {
+                // 刻度
+                show: false // 不显示刻度线
+              },
+              splitLine: { show: false } // 去除网格分割线
+            }
+          ],
+
+          yAxis: [
+            {
+              type: 'value',
+              name: this.yName1,
+              min: 0,
+              max: 1500,
+              interval: 500,
+              axisLabel: {
+                formatter: function(value) {
+                  const val = value + ''
+                  if (val.length > 3) {
+                    return val.substr(0, 1) + ',' + val.substr(1)
+                  } else {
+                    return val
+                  }
+                },
+                textStyle: {
+                  color: '#56565680',
+                  fontSize: '10'
+                },
+                margin: 4
+              },
+              axisLine: {
+                // 线
+                show: false
+              },
+              axisTick: {
+                // 刻度
+                show: false
+              },
+              splitLine: {
+                lineStyle: {
+                  color: '#70707099',
+                  type: 'dashed'
+                }
+              }
+            }
+          ],
+          series: [
+            {
+              name: this.dataName1,
+              type: 'bar',
+              data: this.data1,
+              itemStyle: {
+                normal: {
+                  color: '#FF8181', // 设置柱子颜色
+                  label: {
+                    show: false, // 柱子上显示值
+                    position: 'top', // 值在柱子上方显示
+                    textStyle: {
+                      color: '#f1835b80', // 值得颜色
+                      fontSize: '9px'
+                    },
+                    formatter: function(value) {
+                      const val = value.data + ''
+                      if (val.length > 3) {
+                        return (
+                          '￥' +
+                          val.substr(0, val.length - 3) +
+                          ',' +
+                          val.substr(val.length - 3)
+                        )
+                      } else {
+                        return '￥' + val
+                      }
+                    }
+                  }
+                }
+              },
+              barWidth: 8 // 设置柱子宽度，单位为px
+            },
+
+            {
+              name: this.dataName2,
+              type: 'bar',
+              data: this.data2,
+              itemStyle: {
+                normal: {
+                  color: '#56BE9B', // 设置柱子颜色
+                  label: {
+                    show: true, // 柱子上显示值
+                    position: 'top', // 值在柱子上方显示
+                    textStyle: {
+                      color: '#343434', // 值得颜色
+                      fontSize: '14px'
+                    }
+                  }
+                }
+              },
+              barWidth: 8 // 设置柱子宽度，单位为px
+            },
+
+            {
+              name: this.dataName3,
+              type: 'bar',
+              data: this.data3,
+              itemStyle: {
+                normal: {
+                  color: '#F8C498', // 设置柱子颜色
+                  label: {
+                    show: false, // 柱子上显示值
+                    position: 'top', // 值在柱子上方显示
+                    textStyle: {
+                      color: '#B82B43' // 值得颜色
+                    }
+                  }
+                }
+              },
+              barWidth: 8 // 设置柱子宽度，单位为px
+            }
+          ]
+        }
+      }
+      myChart.setOption(option)
+    }
+  },
+  mounted() {
+    this.drawChart()
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.barCharts {
+  height: 300px;
+}
+</style>
--- src/views/flow/components/thirdTab.vue
+++ src/views/flow/components/thirdTab.vue
@@ -1,95 +1,150 @@
 <template>
-  <div>
+  <div class="thirdTab">
     <!-- 日期选择 -->
     <DateSelect></DateSelect>
-    <!-- 饼图 -->
-    <div class="annular">
-      <h3>客群分析</h3>
-      <van-row class="annularRow">
-        <van-col span="12">
-          <AnnularChart
-            :series="item.data"
-            :legend="[]"
-            :seriesName="item.seriesName"
-          ></AnnularChart
-        ></van-col>
-        <van-col span="12" class="typeCol">
-          <template v-for="(type, tid) in item.data">
-            <van-col span="24" class="typeData" :key="tid">
-              <div class="round" style="border: 1px solid #c32c1c;"></div>
-              <span>{{ type.name }}</span>
-              <span>{{ type.percent }}</span>
-              <span>{{ type.value }}</span>
-              人
+
+    <Module :title="features.title" :nav="features.nav" class="feature">
+      <template slot="context">
+        <!-- 饼图 -->
+        <div class="annular">
+          <van-row class="annularRow">
+            <van-col span="12" class="annularCol">
+              <AnnularChart
+                :series="features.data"
+                :legend="[]"
+                :seriesName="features.seriesName"
+              ></AnnularChart>
+              <div class="annularPeople">
+                <span class="annularPeopleCount">247人</span>
+                <p class="annularPeopleCountTitle">总人数</p>
+              </div>
             </van-col>
-          </template>
-        </van-col>
-      </van-row>
-      <!-- progress进度条 -->
-      <div class="proBgc">
-        <template v-for="(pro, pid) in item.progressDate">
+            <van-col span="12" class="typeCol">
+              <template v-for="(type, tid) in features.data">
+                <van-col span="24" class="typeData" :key="tid">
+                  <div
+                    class="round"
+                    :style="{ backgroundColor: type.color }"
+                  ></div>
+                  <span>{{ type.name }}</span>
+                  <span>{{ type.percent }}</span>
+                  <span>{{ type.value }}</span>
+                  人
+                </van-col>
+              </template>
+            </van-col>
+          </van-row>
+        </div>
+        <template v-for="(pro, pid) in features.progressDate">
           <div class="progress" :key="pid">
+            <!-- progress进度条 -->
             <ProgressItem
               :leftTitle="pro.leftTitle"
               :leftIcon="pro.leftIcon"
+              :leftNumber="pro.leftNumber"
+              :rightTitle="pro.rightTitle"
+              :rightIcon="pro.rightIcon"
+              :rightNumber="pro.rightNumber"
               :percentage="pro.percentage"
               :progressColor="pro.progressColor"
               :trackColor="pro.trackColor"
               :sex="pro.sex"
-              :leftIconColor="pro.leftIconColor"
             ></ProgressItem>
           </div>
         </template>
-      </div>
-    </div>
+      </template>
+    </Module>
+    <Module
+      :title="customerCount.title"
+      :nav="customerCount.nav"
+      class="customer"
+    >
+      <template slot="context">
+        <BarCharts
+          :label="customerCount.label"
+          :xLabel="customerCount.xLabel"
+          :xName="customerCount.xName"
+          :dataName1="customerCount.dataName1"
+          :data1="customerCount.data1"
+          :dataName2="customerCount.dataName2"
+          :data2="customerCount.data2"
+          :dataName3="customerCount.dataName3"
+          :data3="customerCount.data3"
+        ></BarCharts>
+      </template>
+    </Module>
   </div>
 </template>
 
 <script>
+import Module from '@/components/home/module' // 整体模块
 import DateSelect from './dateSelect.vue'
 import AnnularChart from '../../../components/annularCharts'
 import ProgressItem from '../../../components/progressItem'
+import BarCharts from '@/components/flow/barCharts' // 柱状图
 export default {
   components: {
     DateSelect,
     AnnularChart,
-    ProgressItem
+    ProgressItem,
+    Module,
+    BarCharts
   },
   data() {
     return {
-      item: {
-        icon: 'iconkequn',
-        title: '客群特征(今日)',
-        status: '',
+      features: {
+        title: '客群特征',
         type: 'features',
         seriesName: '大数据',
+        nav: 'flow',
         data: [
-          { value: 47, name: '老年', percent: '10.00%' },
-          { value: 154, name: '中年', percent: '20.00%' },
-          { value: 46, name: '青年', percent: '34.00%' },
-          { value: 47, name: '少年', percent: '10.00%' }
+          { value: 47, name: '老年', percent: '10.00%', color: '#F8C498' },
+          { value: 154, name: '中年', percent: '20.00%', color: '#56BE9B' },
+          { value: 46, name: '青年', percent: '34.00%', color: '#F1835B' },
+          { value: 47, name: '少年', percent: '10.00%', color: '#FEA8C5' }
         ],
         progressDate: [
           {
             leftTitle: '男性',
-            leftIcon: 'iconnanren',
+            leftNumber: '203',
+            leftIcon: require('@/assets/icon/manIcon.png'),
+            rightTitle: '女性',
+            rightNumber: '44',
+            rightIcon: require('@/assets/icon/womanIcon.png'),
             percentage: 70,
             trackColor: '#F98181',
             sex: 'woman'
           },
           {
             leftTitle: '回头客',
-            leftIcon: 'iconnanren',
-            percentage: 80
+            leftNumber: '256',
+            leftIcon: require('@/assets/icon/keHu.png'),
+            percentage: 80,
+            progressColor: '#56BE9B'
           },
           {
             leftTitle: '会员',
-            leftIcon: 'iconcanpinhuihuiyuanv6',
+            leftNumber: '165',
+            leftIcon: require('@/assets/icon/huiYuan.png'),
             percentage: 30,
-            progressColor: '#FFCD41',
-            leftIconColor: '#F7B500'
+            progressColor: '#F8C498'
           }
         ]
+      },
+      // 顾客统计
+      customerCount: {
+        img: require('@/assets/icon/price.png'),
+        title: '客流与销售分析',
+        type: 'sale',
+        label: ['回头客', '跨店', '会员'],
+        nav: 'flow',
+        xLabel: ['今日', '日', '六', '歇', '11/12', '11/11'],
+        dataName1: '回头客',
+        data1: [306, 1003, 1103, 1321, 100, 300],
+        dataName2: '跨店',
+        data2: [500, 1300, 1200, 1400, 700, 600],
+        dataName3: '会员',
+        data3: [300, 400, 500, 450, 400, 400]
       }
     }
   }
@@ -97,49 +152,65 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.typeData {
-  padding-bottom: 5px;
-}
-.annular {
-  background-color: #fff;
-  h3 {
-    padding: 10px 0 0 20px;
-  }
-}
-.annularRow {
-  display: flex;
-  align-items: center;
-  // background-color: #fff;
-}
-.typeData {
-  margin: -3px;
-  font-size: 14px;
-  margin-top: 6px;
-  color: #909399;
-  span {
-    margin: 0 4px;
-  }
-}
-.round {
-  display: inline-block;
-  border: 1px solid #c32c1c;
-  background-color: #ffffff;
-  width: 8px;
-  height: 8px;
-  border-radius: 50%;
-  margin-right: 2px;
-}
-.typeCol {
-  padding-bottom: 20px;
-}
 .progress {
-  margin-top: 10px;
+  padding: 19px 13px 0;
 }
-.progress:first-child {
-  margin-top: 0;
-}
-.proBgc {
-  background-color: #fff;
-  padding-bottom: 20px;
+.thirdTab {
+  padding-bottom: 16px;
+  .feature {
+    background-color: #fff;
+  }
+  .module {
+    border-radius: 0 0 10px 10px;
+  }
+  .annular {
+    background-color: #fff;
+    .round {
+      display: inline-block;
+      width: 10px;
+      height: 10px;
+      border-radius: 50%;
+      margin-right: 8px;
+      background-color: #f8c498;
+    }
+    .typeData {
+      margin-top: 7px;
+      font-size: 14px;
+      color: #909399;
+      span {
+        margin: 0 4px;
+        color: #343434;
+        font-size: 12px;
+      }
+    }
+    .annularRow {
+      display: flex;
+      align-items: center;
+      .annularCol {
+        position: relative;
+        .annularPeople {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          .annularPeopleCount,
+          .annularPeopleCountTitle {
+            color: #343434;
+            font-size: 18px;
+            font-weight: bold;
+          }
+          .annularPeopleCountTitle {
+            font-size: 14px;
+            font-weight: 400;
+          }
+        }
+      }
+    }
+  }
+  .customer {
+    margin-top: 12px;
+    background-color: #fff;
+    border-radius: 10px;
+  }
 }
 </style>
