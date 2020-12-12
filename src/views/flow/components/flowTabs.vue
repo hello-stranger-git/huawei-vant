4e12d35cdef9330dc044f957f26b706f254bc8e3
LCP <1012928669@qq.com>
2020-12-12 11:14:54 +0800

    修改了客流页日期选择组件的样式

--- src/views/flow/components/flowTabs.vue
+++ src/views/flow/components/flowTabs.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="navBarItem">
+  <div style="padding: 0 12px">
     <van-row type="flex" justify="space-between">
       <!-- 四个button -->
       <van-col v-for="(item, i) in tabsArr" :key="i">
@@ -17,8 +17,10 @@
         </div>
       </van-col>
     </van-row>
-    <!-- 动态组件 -->
-    <component :is="activeTabArr[activeTabIndex]"></component>
+    <div class="navBarItem">
+      <!-- 动态组件 -->
+      <component :is="activeTabArr[activeTabIndex]"></component>
+    </div>
   </div>
 </template>
 
@@ -76,48 +78,48 @@ export default {
 
 <style lang="less" scoped>
 .navBarItem {
-  margin: 20px 12px 0;
+  // margin: 20px 12px 0;
   background-color: #eee;
   border-radius: 8px;
   overflow: hidden;
 }
-.tabBox.activeClass {
-  background-color: #fff;
-}
-.tabBox {
-  background-color: #c9c9c9;
-  height: 80px;
-  text-align: center;
-  line-height: 70px;
-  position: relative;
-  border-radius: 10px 10px 0 0;
-  border: 1px solid #50a6ff;
-  border-bottom: none;
-  margin: 0px -1px 0px 0px;
-  .iconBox {
-    width: 40px;
-    height: 40px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 15px;
-    position: absolute;
-    left: 50%;
-    top: 40%;
-    margin: -20px 0 0 -20px;
-  }
-  p {
-    width: 100%;
-    height: 8px;
-    font-size: 14px;
-    position: absolute;
-    left: 50%;
-    bottom: 50%;
-    transform: translate(-50%, -50%);
-  }
-}
+// .tabBox.activeClass {
+//   background-color: #fff;
+// }
+// .tabBox {
+//   background-color: #c9c9c9;
+//   height: 80px;
+//   text-align: center;
+//   line-height: 70px;
+//   position: relative;
+//   border-radius: 10px 10px 0 0;
+//   border: 1px solid #50a6ff;
+//   border-bottom: none;
+//   margin: 0px -1px 0px 0px;
+//   .iconBox {
+//     width: 40px;
+//     height: 40px;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     border-radius: 15px;
+//     position: absolute;
+//     left: 50%;
+//     top: 40%;
+//     margin: -20px 0 0 -20px;
+//   }
+//   p {
+//     width: 100%;
+//     height: 8px;
+//     font-size: 14px;
+//     position: absolute;
+//     left: 50%;
+//     bottom: 50%;
+//     transform: translate(-50%, -50%);
+//   }
+// }
 .navBox {
-  margin-bottom: 10px;
+  margin: 20px 0 10px;
   .button {
     font-size: 12px;
     line-height: 17px;
