59580aeb155dc37e40235bb39ee13f1d04fdac1b
xyc <1365884247@qq.com>
2020-12-14 09:54:57 +0800

    将客流页面的navBar去掉

--- src/views/flow/index.vue
+++ src/views/flow/index.vue
@@ -1,16 +1,13 @@
 <template>
   <div style="background:#eee">
-    <NavBar></NavBar>
     <FlowTabs></FlowTabs>
   </div>
 </template>
 
 <script>
-import NavBar from '@/layout/components/navBar'
 import FlowTabs from './components/flowTabs.vue'
 export default {
   components: {
-    NavBar,
     FlowTabs
   }
 }
