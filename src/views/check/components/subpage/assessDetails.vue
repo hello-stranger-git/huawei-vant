e81962d1de979ca4101aed943c16f8c98108b77c
LZ <2706253331@qq.com>
2020-12-18 17:50:44 +0800

    考评详情页完成10%

--- src/views/check/components/subpage/assessDetails.vue
+++ src/views/check/components/subpage/assessDetails.vue
@@ -1,1 +1,23 @@
+<template>
+  <div class="assessDetails">
+    <!-- 头部区域 -->
+    <TopMessage></TopMessage>
+    <!-- 通知栏 -->
+    <Notice></Notice>
+    <!-- 点检屏幕区域 -->
+    <VideoImage></VideoImage>
+  </div>
+</template>
+<script>
+import TopMessage from '@/components/top' // 顶部信息
+import Notice from '@/layout/components/notice' // 通知栏
+import VideoImage from '@/components/check/subpage/videoImage.vue' // 点检截图
 
+export default {
+  components: {
+    TopMessage,
+    Notice,
+    VideoImage
+  }
+}
+</script>
