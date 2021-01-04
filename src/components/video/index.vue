475ae74150834799770b48dbf0c9fef6558c4a47
LZ <2706253331@qq.com>
2021-01-04 20:28:06 +0800

    完成考评详情页面

---
+++ src/components/video/index.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="videoRegion">
+    <!-- 视频 -->
+    <div class="video">
+      <img :src="screen" />
+    </div>
+    <!-- 视频按钮 -->
+    <div class="button_top">
+      <div @click="upPage">
+        <img :src="jiantou" width="30px" height="30px" />
+      </div>
+      <div><img :src="fullScreen" width="33px" height="33px" /></div>
+    </div>
+    <div class="button_content"><img :src="play" /></div>
+    <div class="button_bottom">
+      <div>
+        <img :src="voice" />
+      </div>
+      <div>
+        <button>标清</button>
+        <button>0.5X</button>
+        <button>1X</button>
+        <button>2X</button>
+        <button>4X</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    video: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      screen: this.video, // 点检图
+      voice: require('@/assets/icon/check/checkItem/voice.png'), // 音量图标
+      fullScreen: require('@/assets/icon/check/checkItem/fullScreen.png'), // 全屏图标
+      play: require('@/assets/icon/check/checkItem/play.png'), // 播放图标
+      jiantou: require('@/assets/icon/customerInfo/jiantou.png') // 播放图标
+    }
+  },
+  methods: {
+    upPage() {
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+// 视屏样式
+.videoRegion {
+  position: relative;
+  .video {
+    height: 205px;
+    img {
+      width: 100%;
+    }
+  }
+
+  // 视频按钮样式
+  .button_top,
+  .button_bottom {
+    display: flex;
+    justify-content: space-between;
+    position: absolute;
+    width: 100%;
+  }
+  .button_top {
+    top: 0px;
+    div {
+      width: 33px;
+      height: 33px;
+      margin: 4px 12px;
+    }
+  }
+  .button_content {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 44px;
+    height: 44px;
+    border-radius: 50%;
+    img {
+      width: 100%;
+    }
+  }
+  .button_bottom {
+    align-items: center;
+    bottom: 0px;
+    div {
+      margin: 0 12px;
+      img {
+        width: 30px;
+        height: 30px;
+      }
+
+      button {
+        border-radius: 5px;
+        color: #fff;
+        width: 32px;
+        height: 19px;
+        font-size: 10px;
+        line-height: 19px;
+        margin-left: 10px;
+        padding: 0;
+        background: initial;
+        border: 1px solid rgb(223, 223, 223);
+        outline: none;
+      }
+      button:hover {
+        color: #000;
+        background-color: #fff;
+      }
+    }
+  }
+}
+</style>
--- src/views/check/components/subpage/evaluationDetails.vue
+++ src/views/check/components/subpage/evaluationDetails.vue
@@ -1,4 +1,137 @@
 <template>
   <!-- 考评详情 -->
-  <div>考评详情</div>
+  <div class="evaluationDetails">
+    <!-- 视屏 -->
+    <Video :video="video1" />
+    <!-- 店名 -->
+    <div class="storeName">
+      <h3>重庆石桥铺雨林商都店</h3>
+      <span>100</span>
+      <p>考评总分：100</p>
+    </div>
+    <!-- 合格项 -->
+    <ul>
+      <li>
+        <span class="colorRed">34</span>
+        <p>合格</p>
+      </li>
+      <li>
+        <span>0</span>
+        <p>不合格</p>
+      </li>
+      <li>
+        <span>0</span>
+        <p>不适用</p>
+      </li>
+    </ul>
+    <!-- 考评详情 -->
+    <div class="details">
+      <div>
+        <p>巡店人：OPPO体专店</p>
+        <p>整改：不需要整改</p>
+      </div>
+    </div>
+    <!-- 页尾按钮区域 -->
+    <div class="footer">
+      <div>
+        <van-button>查看详情</van-button>
+        <van-button :to="{ name: 'Check' }">继续巡店</van-button>
+      </div>
+    </div>
+  </div>
 </template>
+<script>
+import Video from '@/components/video' // 视屏区域
+export default {
+  components: {
+    Video
+  },
+  data() {
+    return {
+      // 视屏
+      video1: require('@/assets/videoImage/videoTest.png')
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.evaluationDetails {
+  background-color: #fff;
+  height: 100%;
+}
+.colorRed {
+  color: #e60012;
+}
+// 店名区域样式
+.storeName {
+  padding: 24px 0 14px;
+  text-align: center;
+  h3 {
+    font-size: 16px;
+  }
+  span {
+    display: block;
+    margin-top: 10px;
+    font-size: 24px;
+    line-height: 28px;
+    color: #e60012;
+  }
+  p {
+    margin-top: 4px;
+    font-size: 12px;
+    color: #141414;
+  }
+}
+// 合格区域样式
+ul {
+  border-top: 12px solid #efefef;
+  padding: 20px 40px 16px;
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+  & > li {
+    span {
+      font-size: 18px;
+      line-height: 25px;
+    }
+    p {
+      line-height: 17px;
+      font-size: 12px;
+      margin-top: 2px;
+    }
+  }
+}
+// 考评详情区域样式
+.details > div {
+  background-color: #fff;
+  border-top: 12px solid #efefef;
+  padding: 26px 40px;
+  display: flex;
+  justify-content: space-between;
+  p {
+    font-size: 12px;
+    color: #141414;
+  }
+}
+// 页尾按钮区域样式
+.footer {
+  position: absolute;
+  width: 100%;
+  bottom: 24px;
+  & > div {
+    padding: 0 70px;
+    display: flex;
+    justify-content: space-between;
+    .van-button {
+      width: 100px;
+      height: 39px;
+      background-color: #ff7b40;
+      color: #fff;
+      border-radius: 23px;
+      &:nth-child(2) {
+        background-color: #4a92ff;
+      }
+    }
+  }
+}
+</style>
