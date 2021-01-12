c51540f2c2da7d4222ce7d0ec89c5d29cf71a248
xyc <1365884247@qq.com>
2021-01-12 14:19:07 +0800

    添加统计巡查，模板选择

--- src/views/home/checkCount/index.vue
+++ src/views/home/checkCount/index.vue
@@ -2,6 +2,31 @@
   <div style="background:#eee">
     <!-- 头部区域 -->
     <van-nav-bar title="巡查统计" left-arrow @click-left="onClickLeft" />
+    <div class="select">
+      <span class="title">选择模板</span>
+      <div class="select" @click="openSelect">
+        <span>{{ modulName }}</span>
+        <img
+          :src="arrow"
+          width="14px"
+          height="14px"
+          style="vertical-align: middle;"
+        />
+      </div>
+    </div>
+
+    <!--模板选择-->
+    <van-action-sheet v-model="show" class="showSelectModule" title="选择模板">
+      <div
+        class="content"
+        v-for="(item, index) in moduleData"
+        :key="index"
+        @click="selectModule(item)"
+      >
+        {{ item }}
+      </div>
+    </van-action-sheet>
+
     <div class="top">
       <div class="titleDiv">
         <span class="titleSpan">近三十天巡查项概述</span>
@@ -156,6 +181,7 @@
 export default {
   data() {
     return {
+      arrow: require('@/assets/icon/home/video/videoItem/arrow.png'),
       scoringRate: 0, // 得分率百分比
       evaluationRate: 0, // 考评率百分比
       checkRate: 0, // 点检率百分比
@@ -257,12 +283,26 @@ export default {
           trackColor: '#EFEFEF',
           strokeWidth: '14px'
         }
+      ],
+      show: false,
+      modulName: '超级旗舰店模板',
+      moduleData: [
+        '标杆店及购物中心店模板',
+        '超级旗舰店模板',
+        '吉林省视频巡查模板1.0（吉林地区）'
       ]
     }
   },
   methods: {
     onClickLeft() {
       this.$router.go(-1)
+    },
+    selectModule(data) {
+      this.modulName = data
+      this.show = false
+    },
+    openSelect() {
+      this.show = true
     }
   },
   computed: {
@@ -426,6 +466,47 @@ export default {
   .inspection {
     padding-bottom: 18px;
     border-radius: 0 0 10px 10px;
+  }
+}
+
+//模板选择
+.select {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fff;
+  padding: 12px 6px 6px 12px;
+  .title {
+    font-size: 14px;
+    font-weight: 400;
+    color: #141414;
+  }
+  .select {
+    span {
+      font-size: 12px;
+      font-weight: 400;
+      color: #343434;
+      opacity: 0.5;
+    }
+  }
+}
+
+// 模板选择弹出框
+.showSelectModule {
+  /deep/.van-action-sheet__header {
+    font-size: 18px;
+    font-weight: 800;
+    color: #000000;
+  }
+  .content {
+    text-align: center;
+    &:first-child {
+      margin-top: 40px;
+    }
+    &:last-child {
+      margin-bottom: 24px;
+    }
+    margin-top: 24px;
   }
 }
 </style>
